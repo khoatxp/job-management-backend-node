a2083d4cacff04de63028e99cc52a72d3ad99a11
rthanki <rthanki@sfu.ca>
2020-11-04 15:40:57 -0800

    Create Post button now routes to jobForm.html page

--- formHandler.js
+++ formHandler.js
@@ -4,9 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = 5000;
 
+app.use(express.static(__dirname));
+
 app.get('/',(req, res) =>{
     res.sendFile(path.join(__dirname,'createPostPage.html'));
 });
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT} ` ));
+
+
