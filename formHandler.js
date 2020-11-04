4427eb9475ffc549d7deecfadf12961617610cce
rthanki <rthanki@sfu.ca>
2020-11-04 13:42:41 -0800

    form handler created and page that occurs before job post creation, the one with create post button. Still need to add event listener on create post button and create a table that contains the list of posts created by a user.

---
+++ formHandler.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/',(req, res) =>{
+    res.sendFile(path.join(__dirname,'createPostPage.html'));
+});
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT} ` ));
