8d166a6e70084ec86cbd2c52f401fb726e6a382e
jycasas <jan_ycasas@sfu.ca>
2020-12-01 20:35:58 -0800

    Implement resume storage

--- resume_service/utilities/rest_caller.js
+++ resume_service/utilities/rest_caller.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
 module.exports = {
-    post_call: function (url, contents) {
+    post_call: function (url, contents, headers) {
         return new Promise(function (resolve, reject) {
             axios({
                 method: 'post',
                 url: url,
                 data: contents
             }).then((response) => {
-                resolve()
+                resolve(response.data)
             }, (err) => {
                 reject(err)
             });
